742322336315941a6ec19425fa06ec61e8e94d7a
pacolopez16 <116556032+pacolopez16@users.noreply.github.com>
2025-01-27 13:20:20 +0100

    Update config.php

--- proyectoiaw/config.php
+++ proyectoiaw/config.php
@@ -1,16 +1,1 @@
-<?php
-// config.php
-$servername = "sql308.thsite.top";
-$username = "thsi_38097489";
-$password = "MAxXJ8!0";
-$dbname = "thsi_38097489_bdpaco";
-
-// Crear conexión
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Verificar conexión
-if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
-}
-?>
 
